598a8c6e4f76548b5a7428e5c33343e0369f7068
Jakub Vodak <jakub.vodak@strv.com>
2018-07-12 16:18:54 +0200

    Welcome view

---
+++ CoolChat/Controller/WelcomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeViewController.swift
+//  CoolChat
+//
+//  Created by Jakub Vodak on 12/07/2018.
+//  Copyright © 2018 ICT. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Ahoj")
+
+    }
+
+}
