0e6b0f2c177316e99878584d2e787e9198424742
Jakub Vodak <jakub.vodak@strv.com>
2018-07-13 08:56:53 +0200

    Day 2

--- CoolChat/Controller/WelcomeViewController.swift
+++ CoolChat/Controller/WelcomeViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet var label: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("Ahoj jak se mas Michale")
+
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(WelcomeViewController.titleTap))
+
+        label.addGestureRecognizer(tap)
+
     }
 
+
+
+    @IBAction func titleTap() {
+
+        label.textColor = .green
+    }
+
+
+
+    
 }
